357f40b204dd063c010ac18ed29da9f8e18058c4
Benson-Chen-52 <bensonc52@nycstudents.net>
2024-09-10 16:57:38 -0400

    Lab 1

---
+++ Chen_Benson_Lab_1.cpp
@@ -0,0 +1,38 @@
+// Benson Chen      In-Class Lab 1: cout Statements      9/10/2024 //
+#include <iostream>
+using namespace std;
+
+// beginning of code //
+int main()
+{
+    // series of cout commands //
+    cout << "#######################################################################################" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                           #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#            Press the key combination Ctrl + Shift + Escape                          #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager”  #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#            Type “Task Manager” in the Windows Start menu search                     #" << endl;
+    cout << "#                                                                                     #" << endl;
+    cout << "#######################################################################################" << "\n";
+
+    // return statement //
+    return 0;
+}
+
+/*[Running] cd "c:\Users\college\Documents\GitHub\lab-1-cout-statements-Benson-Chen-52\" && g++ Chen_Benson_Lab_1.cpp -o Chen_Benson_Lab_1 && "c:\Users\college\Documents\GitHub\lab-1-cout-statements-Benson-Chen-52\"Chen_Benson_Lab_1
+############################################################
+
+Ways to access the Task Manager on your Windows computer:
+
+           Press the key combination Ctrl + Shift + Escape
+
+           Press the key combination Ctrl + Alt + Delete and select “Task Manager”
+
+           Type “Task Manager” in the Windows Start menu search
+
+############################################################
+
+[Done] exited with code=0 in 1.766 seconds*/
